7d1aaa10b9ee47bf7930c46dde5f8b7620ec625e
MGross21 <mgstinger521@gmail.com>
2025-10-12 00:19:39 -0700

    Refactor and lint Pose class - Remove duplicated arithmetic methods - Improve math operations with kotlin core lib - convert to data class. removes need for copy method. - convert getLength to length attribute getter - Reduce nesting error throws

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/localization/Pose.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/localization/Pose.kt
@@ -1,9 +1,9 @@
 package org.firstinspires.ftc.teamcode.localization
 
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit
+import kotlin.math.abs
+import kotlin.math.atan2
 import kotlin.math.cos
-import kotlin.math.pow
+import kotlin.math.hypot
 import kotlin.math.sin
 import kotlin.math.sqrt
 
@@ -11,119 +11,57 @@ import kotlin.math.sqrt
  * Represents a 2D pose with x, y coordinates and a heading (angle).
  * Provides methods for vector operations, distance calculations, and equality checks.
  */
-class Pose(var x: Double = 0.0, var y: Double = 0.0, var heading: Double = 0.0) {
-    // Vector operations
-    fun add(other: Pose) {
-        this.x += other.x
-        this.y += other.y
-        this.heading += other.heading
-    }
-
-    fun subtract(other: Pose) {
-        this.x -= other.x
-        this.y -= other.y
-        this.heading -= other.heading
-    }
-
-    fun scale(scalar: Double, scaleHeading: Boolean = false) {
-        this.x *= scalar
-        this.y *= scalar
-        if (scaleHeading) {
-            this.heading *= scalar
-        }
-    }
-
-    fun divide(scalar: Double) {
-        if (scalar != 0.0) {
-            this.x /= scalar
-            this.y /= scalar
-            this.heading /= scalar
-        } else {
-            throw IllegalArgumentException("Cannot divide by zero")
-        }
-    }
-
-    fun rotate(angle: Double) {
-        // Rotate about the origin (0, 0)
+data class Pose(
+    val x: Double = 0.0,
+    val y: Double = 0.0,
+    val heading: Double = 0.0,
+) {
+    fun rotate(angle: Double): Pose {
+        // Rotate the (x, y) vector by the given angle in radians
         val cosTheta = cos(angle)
         val sinTheta = sin(angle)
-        val newX = this.x * cosTheta - this.y * sinTheta
-        val newY = this.x * sinTheta + this.y * cosTheta
-        this.x = newX
-        this.y = newY
+        val newX = x * cosTheta - y * sinTheta
+        val newY = x * sinTheta + y * cosTheta
+        return Pose(newX, newY, heading)
     }
 
-    fun normalize() : Pose {
-        val length = this.getLength()
-        return if (length > 0) {
-            Pose(this.x / length, this.y / length, this.heading)
+    fun normalize(): Pose {
+        val len = length
+        return if (len > 0) {
+            Pose(x / len, y / len, heading)
         } else {
-            Pose(0.0, 0.0, this.heading) // Return a zero vector with the same heading
+            Pose(0.0, 0.0, heading) // Return a zero vector with the same heading
         }
     }
 
     // Overloaded operators for vector operations
-    operator fun plus(other: Pose): Pose {
-        return Pose(this.x + other.x, this.y + other.y, this.heading + other.heading)
-    }
+    operator fun plus(other: Pose): Pose = Pose(x + other.x, y + other.y, heading + other.heading)
 
-    operator fun minus(other: Pose): Pose {
-        return Pose(this.x - other.x, this.y - other.y, this.heading - other.heading)
-    }
+    operator fun minus(other: Pose): Pose = Pose(x - other.x, y - other.y, heading - other.heading)
 
-    operator fun times(scalar: Double): Pose {
-        return Pose(this.x * scalar, this.y * scalar, this.heading * scalar)
-    }
-
-    operator fun times(otherPose: Pose): Pose {
-        // Element-wise multiplication
-        return Pose(this.x * otherPose.x, this.y * otherPose.y, this.heading * otherPose.heading)
-    }
+    operator fun times(scalar: Double): Pose = Pose(x * scalar, y * scalar, heading)
 
     operator fun div(scalar: Double): Pose {
-        if (scalar != 0.0) {
-            return Pose(this.x / scalar, this.y / scalar, this.heading / scalar)
-        } else {
-            throw IllegalArgumentException("Cannot divide by zero")
-        }
+        require(scalar != 0.0) { "Cannot divide by zero" }
+        return Pose(x / scalar, y / scalar, heading)
     }
 
     // Other utility methods
-    fun getLength(): Double {
-        return sqrt(x * x + y * y)
-    }
+    val length: Double get() = sqrt(x * x + y * y)
 
-    fun distanceTo(other: Pose): Double {
-        return sqrt((other.x - this.x).pow(2.0) + (other.y - this.y).pow(2.0))
-    }
+    fun distanceTo(other: Pose): Double = hypot(other.x - x, other.y - y)
 
-    fun roughlyEquals(other: Pose, positionTolerance: Double = 0.001, headingTolerance: Double = 0.001): Boolean {
-        return this.distanceTo(other) < positionTolerance &&
-                kotlin.math.abs(this.heading - other.heading) < headingTolerance
-    }
+    fun angleTo(other: Pose): Double = atan2(other.y - y, other.x - x)
 
-    override fun toString(): String {
-        return "(x=${"%.3f".format(x)}, y=${"%.3f".format(y)}, heading=${"%.3f".format(heading)})"
-    }
+    fun roughlyEquals(
+        other: Pose,
+        positionTolerance: Double = 0.001,
+        headingTolerance: Double = 0.001,
+    ): Boolean =
+        distanceTo(other) < positionTolerance &&
+            abs(heading - other.heading) < headingTolerance
 
-    fun toDesmosString(): String {
-        return "(${"%.3f".format(x)}, ${"%.3f".format(y)})"
-    }
+    override fun toString(): String = "Pose(x=${"%.3f".format(x)}, y=${"%.3f".format(y)}, heading=${"%.3f".format(heading)})"
 
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other !is Pose) return false
-        return this.x == other.x && this.y == other.y && this.heading == other.heading
-    }
-
-    fun copy(): Pose {
-        return Pose(x, y, heading)
-    }
-
-    override fun hashCode(): Int {
-        var result = x.hashCode()
-        result = 31 * result + y.hashCode()
-        result = 31 * result + heading.hashCode()
-        return result
-    }
+    fun toDesmosString(): String = "(${"%.3f".format(x)}, ${"%.3f".format(y)})"
 }
