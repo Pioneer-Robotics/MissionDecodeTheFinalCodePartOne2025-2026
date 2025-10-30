ad1cb25102bb432b8a08dd7417fb5b8496279f16
MGross21 <mgstinger521@gmail.com>
2025-10-29 18:52:27 -0700

    Refactor FlywheelImpl to accept motor name as a parameter

--- TeamCode/src/main/kotlin/pioneer/hardware/impl/FlywheelImpl.kt
+++ TeamCode/src/main/kotlin/pioneer/hardware/impl/FlywheelImpl.kt
@@ -4,12 +4,12 @@ import com.qualcomm.robotcore.hardware.DcMotor
 import com.qualcomm.robotcore.hardware.DcMotorEx
 import com.qualcomm.robotcore.hardware.DcMotorSimple
 import com.qualcomm.robotcore.hardware.HardwareMap
-import pioneer.Constants
+import pioneer.constants.HardwareNames
 import pioneer.hardware.interfaces.Flywheel
 import kotlin.jvm.java
 
-class FlywheelImpl (hardwareMap: HardwareMap) : Flywheel {
-    val flywheel = hardwareMap.get(DcMotorEx::class.java, Constants.HardwareNames.FLYWHEEL)
+class FlywheelImpl (hardwareMap: HardwareMap, name: String = "flywheel") : Flywheel {
+    val flywheel = hardwareMap.get(DcMotorEx::class.java, name)
 
     init {
         flywheel.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
