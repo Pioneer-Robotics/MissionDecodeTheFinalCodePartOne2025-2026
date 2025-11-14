f67a4cb6a261435326ecf385d88b8ceceeb2e1f0
MGross21 <mgstinger521@gmail.com>
2025-11-13 21:32:30 -0700

    Refactor AprilTagMeta interface and classes

--- TeamCode/src/main/kotlin/pioneer/decode/AprilTagMeta.kt
+++ TeamCode/src/main/kotlin/pioneer/decode/AprilTagMeta.kt
@@ -2,44 +2,35 @@ package pioneer.decode
 
 import org.firstinspires.ftc.robotcore.external.matrices.VectorF
 import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase
+import pioneer.general.AllianceColor
 
-interface AprilTagMeta{
-    val name: String
+private val decodeTagLibrary by lazy { AprilTagGameDatabase.getDecodeTagLibrary() }
+
+interface AprilTagMeta {
     val id: Int
-    val color: Color
-    val positionVector: VectorF
-    val position: List<Double>
-
+    val color: AllianceColor
+    val name: String
+        get() = decodeTagLibrary.lookupTag(id).name
+    val position: VectorF
 }
 
-enum class Color{
-    BLUE,
-    RED,
-    NEUTRAL
+abstract class DecodeAprilTag(
+    override val id: Int,
+    override val color: AllianceColor
+) : AprilTagMeta {
+    override val position: VectorF by lazy {
+        decodeTagLibrary.lookupTag(id).fieldPosition
+    }
 }
 
+// Helper extension function to convert VectorF to List<Double>
+fun VectorF.toList(): List<Double> = listOf(this[0].toDouble(), this[1].toDouble(), this[2].toDouble())
+fun VectorF.x() = this[0]
+fun VectorF.y() = this[1]
+fun VectorF.z() = this[2]
 
-class blueGoalTag: AprilTagMeta{
-    override val name = "Blue Goal Tag"
-    override val id = 20
-    override val color = Color.BLUE
-    override val positionVector = AprilTagGameDatabase.getDecodeTagLibrary().lookupTag(id).fieldPosition
-    override val position = listOf(positionVector[0], positionVector[1], positionVector[2])
-}
+class BlueGoalTag : DecodeAprilTag(20, AllianceColor.BLUE)
 
-class redGoalTag: AprilTagMeta{
-    override val name = "Red Goal Tag"
-    override val id = 24
-    override val color = Color.RED
-    override val positionVector = AprilTagGameDatabase.getDecodeTagLibrary().lookupTag(this.id).fieldPosition
-    override val position = listOf(positionVector[0], positionVector[1], positionVector[2])
-}
+class RedGoalTag : DecodeAprilTag(24, AllianceColor.RED)
 
-class obeliskTag: AprilTagMeta{
-    override val name = "Obelisk Tag"
-    override val id = 24
-    override val color = Color.NEUTRAL
-    override val positionVector = AprilTagGameDatabase.getDecodeTagLibrary().lookupTag(this.id).fieldPosition
-    override val position = listOf(positionVector[0], positionVector[1], positionVector[2])
-
-}
+// class ObeliskTag : AprilTag(0, AllianceColor.NEUTRAL)
