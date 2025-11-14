bc8657b30db517106460111dca8d112c8304d2d3
MGross21 <mgstinger521@gmail.com>
2025-11-13 21:58:33 -0700

    Refactor AprilTag class so distance and angle unit cannot cause become conflicting

--- TeamCode/src/main/kotlin/pioneer/vision/AprilTag.kt
+++ TeamCode/src/main/kotlin/pioneer/vision/AprilTag.kt
@@ -8,13 +8,32 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit
 import org.firstinspires.ftc.robotcore.external.navigation.Position
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles
 
+/**
+ * AprilTag processor with camera offset configuration.
+ *
+ * @property x The X position offset of the camera (default: 0.0)
+ * @property y The Y position offset of the camera (default: 0.0)
+ * @property z The Z position offset of the camera (default: 0.0)
+ * @property yaw The yaw angle offset of the camera (default: 0.0)
+ * @property pitch The pitch angle offset of the camera (default: 0.0)
+ * @property roll The roll angle offset of the camera (default: 0.0)
+ * @property distanceUnit The unit for distance measurements (default: CM)
+ * @property angleUnit The unit for angle measurements (default: RADIANS)
+ * @property draw Whether to draw debug visualizations on the camera stream (default: false)
+ */
 class AprilTag(
-    private val position: Position = Position(DistanceUnit.CM, 0.0, 0.0, 0.0, 0),
-    private val orientation: YawPitchRollAngles = YawPitchRollAngles(AngleUnit.RADIANS, 0.0, 0.0, 0.0, 0),
+    private val x: Double = 0.0,
+    private val y: Double = 0.0,
+    private val z: Double = 0.0,
+    private val yaw: Double = 0.0,
+    private val pitch: Double = 0.0,
+    private val roll: Double = 0.0,
     private val distanceUnit: DistanceUnit = DistanceUnit.CM,
     private val angleUnit: AngleUnit = AngleUnit.RADIANS,
     private val draw: Boolean = false,
 ): Processor {
+    private val position = Position(distanceUnit, x, y, z, 0)
+    private val orientation = YawPitchRollAngles(angleUnit, yaw, pitch, roll, 0)
     private val library: AprilTagLibrary =
         AprilTagLibrary
             .Builder()
