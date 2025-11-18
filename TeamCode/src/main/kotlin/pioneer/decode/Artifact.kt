bc913c68f77aef95a0f1c7a94c76af323daf9516
MGross21 <mgstinger521@gmail.com>
2025-11-17 18:01:45 -0700

    Refactor Artifact enum to simplify Swatch usage

--- TeamCode/src/main/kotlin/pioneer/decode/Artifact.kt
+++ TeamCode/src/main/kotlin/pioneer/decode/Artifact.kt
@@ -1,14 +1,14 @@
 package pioneer.decode
 
 import org.firstinspires.ftc.vision.opencv.ColorRange
-import org.firstinspires.ftc.vision.opencv.PredominantColorProcessor
+import org.firstinspires.ftc.vision.opencv.PredominantColorProcessor.Swatch
 
 enum class Artifact(
     val color: ColorRange,
-    val swatch: PredominantColorProcessor.Swatch
+    val swatch: Swatch
 ) {
-    PURPLE(ColorRange.ARTIFACT_PURPLE, PredominantColorProcessor.Swatch.ARTIFACT_PURPLE),
-    GREEN(ColorRange.ARTIFACT_GREEN, PredominantColorProcessor.Swatch.ARTIFACT_GREEN);
+    PURPLE(ColorRange.ARTIFACT_PURPLE, Swatch.ARTIFACT_PURPLE),
+    GREEN(ColorRange.ARTIFACT_GREEN, Swatch.ARTIFACT_GREEN);
 
     override fun toString() = "$name Artifact"
 }
