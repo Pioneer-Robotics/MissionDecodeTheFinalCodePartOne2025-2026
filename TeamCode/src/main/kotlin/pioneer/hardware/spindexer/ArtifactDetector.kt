f2d8606c114894532862ddb4076db55952e1a13a
braedengracy <braedeng2008@gmail.com>
2026-02-20 21:19:03 -0700

    New color sensor ranges for tournament

--- TeamCode/src/main/kotlin/pioneer/hardware/spindexer/ArtifactDetector.kt
+++ TeamCode/src/main/kotlin/pioneer/hardware/spindexer/ArtifactDetector.kt
@@ -5,10 +5,10 @@ import pioneer.hardware.RevColorSensor
 
 class ArtifactDetector(val sensor: RevColorSensor) {
     fun detect(): Artifact? {
-        if (sensor.distance > 8.0) return null
+        if (sensor.distance > 7.0) return null
         return when (sensor.hue) {
-            in 130.0..170.0 -> Artifact.GREEN
-            in 170.0..250.0 -> Artifact.PURPLE
+            in 145.0..170.0 -> Artifact.GREEN
+            in 195.0..230.0 -> Artifact.PURPLE
             else -> null
         }
     }
